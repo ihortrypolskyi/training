0ee81fcea7a63c16b7e8438b6a161bf782e82ef3
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-04-11 19:34:08 +0300

    static_pages/content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:add_text) {'my site page: '}
   describe "home page" do
       it "should have content 'Sample App'" do
         visit "/static_pages/home"
         expect(page).to have_content('Sample App')
+      end
+      it 'should have title Home' do
+        visit 'static_pages/home'
+        expect(page).to have_title("#{add_text} Home")
       end
   end
 
@@ -19,6 +24,18 @@ RSpec.describe "StaticPages", type: :request do
     it 'should have About Us' do
       visit 'static_pages/about'
       expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe 'contact page' do
+    it 'should have content Contact' do
+      visit 'static_pages/contact'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have title Contact " do
+      visit 'static_pages/contact'
+      expect(page).to have_title("#{add_text} Contact")
     end
   end
 end
