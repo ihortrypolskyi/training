e78fa12941c5eff20fcbd1701efc550caac2fa4c
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-04-13 17:19:18 +0300

    signup resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/new', as: 'signup'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'static_pages/home', as: 'home'
+  get 'static_pages/help', as: 'help'
+  get 'static_pages/about', as: 'about'
+  get 'static_pages/contact', as: 'contact'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,44 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   let(:add_text) {'My training site - '}
+  subject {page}
   describe "home page" do
-      it "should have content 'Sample App'" do
-        visit "/static_pages/home"
-        expect(page).to have_content('Sample App')
-      end
-      it 'should have base title Home' do
-        visit 'static_pages/home'
-        expect(page).to have_title("#{add_text}")
-      end
-
-      it 'should not have custom title Home' do
-        visit 'static_pages/home'
-        expect(page).not_to have_title("Home")
-      end
+    before {visit home_path}
+      it { should have_content('Sample App') }
+      it { should have_title(full_title('')) }
+      it { should_not have_title('Home') }
   end
 
   describe 'help page' do
     it 'should have Sample App' do
-      visit 'static_pages/help'
+      visit help_path
       expect(page).to have_content('Sample App')
     end
   end
 
   describe 'about page' do
     it 'should have About Us' do
-      visit 'static_pages/about'
+      visit about_path
       expect(page).to have_content('Sample App')
     end
   end
 
   describe 'contact page' do
+    before {visit contact_path}
     it 'should have content Contact' do
-      visit 'static_pages/contact'
+
       expect(page).to have_content('Sample App')
     end
 
     it "should have title Contact " do
-      visit 'static_pages/contact'
+
       expect(page).to have_title("#{add_text} Contact")
     end
   end
